8414fb5fc185ee9a8351e14196840c9d64721148
Micha Alt <mail@lean-stack.de>
2021-05-10 21:32:49 +0200

    Initial rendering of an (empty) todo list.

--- src/app/todos/components/todos-shell/todos-shell.component.ts
+++ src/app/todos/components/todos-shell/todos-shell.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import { Todo } from '../../model/todo';
 
 @Component({
   selector: 'todos-shell',
   templateUrl: './todos-shell.component.html'
 })
-export class TodosShellComponent {}
+export class TodosShellComponent {
+
+  author = 'Micha Alt';
+  authorUrl = 'https://github.com/lean-ng';
+
+  todos: Todo[] = [];
+}
