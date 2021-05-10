1d69cbad5bf5ba82c3ead631f074141b79b5e1d6
Micha Alt <mail@lean-stack.de>
2021-05-10 21:35:47 +0200

    Extract 'todos-input' component.

---
+++ src/app/todos/components/todos-input/todos-input.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'todos-input',
+  templateUrl: './todos-input.component.html'
+})
+export class TodosInputComponent {
+
+}
--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TodosShellComponent } from './components/todos-shell/todos-shell.component';
 import { TodosItemComponent } from './components/todos-item/todos-item.component';
+import { TodosInputComponent } from './components/todos-input/todos-input.component';
 
 @NgModule({
   declarations: [
     TodosShellComponent,
-    TodosItemComponent
+    TodosItemComponent,
+    TodosInputComponent
   ],
   imports: [
     CommonModule
