9343c910c455cd03c4c43ad018b542c9b1d0e021
Micha Alt <mail@lean-stack.de>
2021-05-10 20:57:00 +0200

    Extract new component: todos-item

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TodosShellComponent } from './components/todos-shell/todos-shell.component';
+import { TodosItemComponent } from './components/todos-item/todos-item.component';
 
 @NgModule({
   declarations: [
-    TodosShellComponent
+    TodosShellComponent,
+    TodosItemComponent
   ],
   imports: [
     CommonModule
