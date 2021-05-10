3cbbd99e81bf2ae4161a544067ed989c8b87bf10
Micha Alt <mail@lean-stack.de>
2021-05-10 20:34:01 +0200

    Scaffold shell component.

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TodosShellComponent } from './components/todos-shell/todos-shell.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TodosShellComponent
+  ],
   imports: [
     CommonModule
   ]
